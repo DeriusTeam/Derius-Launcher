37a7dd4907810e00f6516924a204883fd4920462
DeriusTeam <derius.team@libero.it>
2024-12-31 11:38:59 +0100

    Update manutenzione.js

--- manutenzione.js
+++ manutenzione.js
@@ -6,13 +6,13 @@ function gestisciManutenzione() {
     // Se la manutenzione è attiva e non siamo già su man.html, reindirizza a man.html
     if (is_manutenzione && !window.location.pathname.endsWith('/man.html')) {
         console.log("Manutenzione attiva, reindirizzamento a man.html");
-        window.location.href = "/man.html";
+        window.location.href = "/Derius-Launcher/man.html";
     }
 
     // Se la manutenzione è disattivata e siamo su man.html, reindirizza a index.html
     if (!is_manutenzione && window.location.pathname.endsWith('/man.html')) {
         console.log("Manutenzione disattivata, reindirizzamento a index.html");
-        window.location.href = "/index.html";
+        window.location.href = "/Derius-Launcher/index.html";
     }
 }
 
