7228550bb24693d3a80d946b8ce82a52e91423af
DeriusTeam <derius.team@libero.it>
2024-12-31 12:44:21 +0100

    Update manutenzione.js

--- manutenzione.js
+++ manutenzione.js
@@ -9,6 +9,8 @@ if (inManutenzione && paginaCorrente !== "man.html") {
     window.location.href = "man.html"; // Reindirizza a man.html se la manutenzione è attiva
 } else if (!inManutenzione && paginaCorrente !== "index.html") {
     window.location.href = "index.html"; // Reindirizza a index.html se la manutenzione non è attiva
-} else if (!inManutenzione && paginaCorrente == "man.html") {
+} 
+
+if (!inManutenzione && paginaCorrente == "man.html") {
     window.location.href = "index.html"; // Reindirizza a index.html se la manutenzione non è attiva
 }
