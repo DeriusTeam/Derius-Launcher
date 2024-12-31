eb8131b73c07ef1daa9ee1607cafd3432510f4c3
DeriusTeam <derius.team@libero.it>
2024-12-31 11:58:56 +0100

    Update manuntenzione.js

--- manuntenzione.js
+++ manuntenzione.js
@@ -1,26 +1,14 @@
-// manutenzione.js
+async function checkManutenzione() {
+    try {
+        const response = await fetch('/config.json');
+        const config = await response.json();
 
-// Variabile che controlla lo stato di manutenzione
-const inManutenzione = true; // Imposta su `true` o `false` in base alle esigenze
-
-// Funzione per reindirizzare l'utente
-function reindirizzaUtente() {
-    // Ottieni il percorso della pagina corrente
-    const paginaCorrente = window.location.pathname.split("/").pop(); // Esempio: "index.html" o "man.html"
-
-    // Controlla se la manutenzione è attiva
-    if (inManutenzione) {
-        // Se l'utente non è già su man.html, reindirizzalo
-        if (paginaCorrente !== "man.html") {
-            window.location.href = "man.html";
+        if (config.is_manutenzione && !window.location.pathname.endsWith('/man.html')) {
+            window.location.href = "/man.html";
         }
-    } else {
-        // Se l'utente non è già su index.html, reindirizzalo
-        if (paginaCorrente !== "index.html") {
-            window.location.href = "index.html";
-        }
+    } catch (error) {
+        console.error("Errore durante il caricamento del file di configurazione:", error);
     }
 }
 
-// Esegui la funzione di reindirizzamento al caricamento della pagina
-reindirizzaUtente();
+checkManutenzione();
