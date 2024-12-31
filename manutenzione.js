82854033b6452583731e1d33a7a7a261c9a962e2
DeriusTeam <derius.team@libero.it>
2024-12-31 12:22:55 +0100

    Update manutenzione.js

--- manutenzione.js
+++ manutenzione.js
@@ -1,9 +1,9 @@
-const manutenzione = true;
+// Variabile che controlla lo stato di manutenzione
+const inManutenzione = true; // Imposta su `true` o `false` in base alle esigenze
 
-async function checkManutenzione() {
-        if (manutenzione && !window.location.pathname.endsWith('/man.html')) {
-            window.location.href = "/man.html";
-        }
+// Reindirizza l'utente in base allo stato di manutenzione
+if (inManutenzione) {
+    window.location.href = "man.html"; // Reindirizza a man.html se la manutenzione è attiva
+} else {
+    window.location.href = "index.html"; // Reindirizza a index.html se la manutenzione non è attiva
 }
-
-checkManutenzione();
