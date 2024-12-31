1cdad0f3140cb8d60f7d76bb8bec89a8790dd5e6
DeriusTeam <derius.team@libero.it>
2024-12-31 10:03:37 +0100

    Update manutenzione.js

--- manutenzione.js
+++ manutenzione.js
@@ -1,10 +1,10 @@
 async function checkManutenzione() {
     try {
-        const response = await fetch('config.json');
+        const response = await fetch('/config.json');
         const config = await response.json();
 
-        if (config.is_manutenzione) {
-            window.location.href = "man.html";
+        if (config.is_manutenzione && !window.location.pathname.endsWith('/man.html')) {
+            window.location.href = "/man.html";
         }
     } catch (error) {
         console.error("Errore durante il caricamento del file di configurazione:", error);
