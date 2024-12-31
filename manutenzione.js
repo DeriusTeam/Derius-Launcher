a9169ca6d191ff4f881c9ce8d1fafa050453b359
DeriusTeam <derius.team@libero.it>
2024-12-31 12:53:54 +0100

    Update manutenzione.js

--- manutenzione.js
+++ manutenzione.js
@@ -1,12 +1,12 @@
 // Variabile che controlla lo stato di manutenzione
 const inManutenzione = false; // Imposta su `true` o `false` in base alle esigenze
 
-// Ottieni il percorso della pagina corrente
-const paginaCorrente = window.location.pathname.split("/").pop(); // Esempio: "index.html" o "man.html"
+// Ottieni l'URL completo della pagina corrente
+const urlCorrente = window.location.href;
 
 // Reindirizza l'utente solo se non è già nella pagina corretta
-if (inManutenzione && paginaCorrente !== "man.html") {
+if (inManutenzione && !urlCorrente.endsWith("/man.html")) {
     window.location.href = "man.html"; // Reindirizza a man.html se la manutenzione è attiva
-} else if (!inManutenzione && paginaCorrente == "man.html") {
+} else if (!inManutenzione && urlCorrente.endsWith("/man.html")) {
     window.location.href = "index.html"; // Reindirizza a index.html se la manutenzione non è attiva
-} 
+}
