b4aa4ef3af7df424a1befb0e6c7c4579214d61b3
DeriusTeam <derius.team@libero.it>
2024-12-31 12:20:31 +0100

    Update manutenzione.js

--- manutenzione.js
+++ manutenzione.js
@@ -1,13 +1,9 @@
 const manutenzione = true;
 
 async function checkManutenzione() {
-    try {
         if (manutenzione && !window.location.pathname.endsWith('/man.html')) {
             window.location.href = "/man.html";
         }
-    } catch (error) {
-        console.error("Errore durante il caricamento del file di configurazione:", error);
-    }
 }
 
 checkManutenzione();
