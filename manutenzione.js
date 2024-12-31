e044ddea67837f7d55303d7673cad780aee6f393
DeriusTeam <derius.team@libero.it>
2024-12-31 12:50:05 +0100

    Update manutenzione.js

--- manutenzione.js
+++ manutenzione.js
@@ -7,6 +7,6 @@ const paginaCorrente = window.location.pathname.split("/").pop(); // Esempio: "i
 // Reindirizza l'utente solo se non è già nella pagina corretta
 if (inManutenzione && paginaCorrente !== "man.html") {
     window.location.href = "man.html"; // Reindirizza a man.html se la manutenzione è attiva
-} else if (!inManutenzione && paginaCorrente !== "man.html") {
+} else if (!inManutenzione && paginaCorrente == "man.html") {
     window.location.href = "index.html"; // Reindirizza a index.html se la manutenzione non è attiva
 } 
